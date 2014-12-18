5a1d068e06d4f2c9958060a1a4f7240967a30fb0
Nick Barrett <nick@oxygem.com>
2014-12-18 23:39:54 +0000

    Finish implementing: stop/control/start/reload/update tasks, handle task exceptions

--- pytask/pytask.py
+++ pytask/pytask.py
@@ -2,9 +2,11 @@
 # File: pytask.py
 # Desc: provides a framework for managing & running greenlet based tasks
 
+import sys
 import time
 import json
 import logging
+import traceback
 from uuid import uuid4
 
 import gevent
@@ -23,28 +25,34 @@ def run_loop(function, interval):
 
 
 class Task(object):
+    '''An individual task base'''
     # Internal task_id
     _id = None
     # Internal task state
     _state = 'WAIT'
-    # PyTask pubsub object
-    _pubsub = None
+    # Redis object
+    _redis = None
     # & channel name
     _channel = None
 
-    # Emit task events -> pubsub channel
     def emit(self, event, data):
-        self._pubsub.publish(self._channel, json.dumps({
+        '''Emit task events -> pubsub channel'''
+        self._redis.publish(self._channel, json.dumps({
             'event': event,
             'data': data
         }))
 
     # Tasks which don't define a stop are assumed not to spawn any sub-greenlets
+    # this is called before we kill the task's greenlet (running task.start)
     def stop(self):
         pass
 
 
 class PyTask(object):
+    '''
+        A daemon that starts/stops tasks & replicates that to a Redis instance
+        tasks can be control via Redis pubsub
+    '''
     REDIS = None
     TASKS = {}
 
@@ -84,8 +92,8 @@ class PyTask(object):
 
 
     ### Public api
-    # Run pytask, basically a wrapper to handle tick count & KeyboardInterrupt
     def run(self, task_map=None):
+        '''Run pytask, basically a wrapper to handle tick count & KeyboardInterrupt'''
         if task_map:
             self.TASKS.update(task_map)
 
@@ -109,21 +117,21 @@ class PyTask(object):
             # ... or the user/process asked us to quit!
             self.logger.info('Exiting upon user command...')
 
-    # Used to start tasks on this worker (no queue), before calling .run
     def pre_start_task(self, task_name, task_data=None):
+        '''Used to start tasks on this worker (no queue), before calling .run'''
         if task_data is None:
             task_data = {}
 
         self.pre_start_tasks.append((task_name, task_data))
 
-    # Add a task class
     def add_task(self, task_class):
+        '''Add a task class'''
         self.TASKS[task_class.NAME] = task_class
 
 
     ### Internal task management
-    # Starts a task on *this* worker
     def _pre_start_task(self, task_name, task_data):
+        '''Starts a task on *this* worker'''
         # Generate task_id
         task_id = str(uuid4())
 
@@ -139,8 +147,8 @@ class PyTask(object):
         # Add the task
         self._add_task(task_id)
 
-    # Interally add a task from the new-task queue
     def _add_task(self, task_id):
+        '''Interally add a task from the new-task queue'''
         self.logger.debug('New task: {0}'.format(task_id))
 
         # Check if task exists, exit if so (assume duplicate queue push)
@@ -166,7 +174,7 @@ class PyTask(object):
             return
 
         task._id = task_id
-        task._pubsub = self.pubsub
+        task._redis = self.redis
         task._channel = '{0}{1}'.format(self.REDIS['TASK_PREFIX'], task_id)
 
         # Add to Redis set
@@ -181,19 +189,10 @@ class PyTask(object):
             'last_update': time.time()
         })
 
-        # Handle control messages
-        def on_control(message):
-            if message == 'stop':
-                self._stop_task(task_id)
-            elif message == 'start':
-                self._start_task(task_id)
-            elif message =='reload':
-                self._reload_task(task_id)
-            else:
-                self.logger.warning('Unknown control command: {0}'.format(message))
-
         # Subscribe to control channel
-        self._subscribe(on_control, channel='{0}{1}-control'.format(
+        self._subscribe(
+            lambda message: self._control_task(task_id, message),
+            channel='{0}{1}-control'.format(
             self.REDIS['TASK_PREFIX'],
             task_id
         ))
@@ -203,31 +202,55 @@ class PyTask(object):
         self._start_task(task_id)
         self.logger.info('Task {0} added with ID {1}'.format(task_class, task_id))
 
-    # Stop a task & remove it's greenlet
-    def _stop_task(self, task_id):
-        # TODO: better check for already-stopped task
+    def _control_task(self, task_id, message):
+        '''Handle control pubsub messages'''
+        if message == 'stop':
+            self._stop_task(task_id)
+        elif message == 'start':
+            self._start_task(task_id)
+        elif message =='reload':
+            self._reload_task(task_id)
+        else:
+            self.logger.warning('Unknown control command: {0}'.format(message))
 
+    def _stop_task(self, task_id):
+        '''Stops a task and kills/removes the greenlet'''
+        if self.tasks[task_id]._state == 'STOPPED': return
         self.logger.debug('Stopping task: {0}'.format(task_id))
 
         self.tasks[task_id].stop()
         self.greenlets[task_id].kill()
+        del self.greenlets[task_id]
 
         # Set STOPPED in task & Redis
         self.tasks[task_id]._state = 'STOPPED'
+        self.redis.hset('{0}{1}'.format(
+            self.REDIS['TASK_PREFIX'],
+            task_id,
+        ), 'state', 'STOPPED')
 
-    # Start a task in a new greenlet
     def _start_task(self, task_id):
-        # TODO: better checks for already-running task
+        '''Starts a task in a new greenlet'''
+        if self.tasks[task_id]._state == 'RUNNING': return
 
         self.logger.debug('Starting task: {0}'.format(task_id))
-        self.greenlets[task_id] = gevent.spawn(self.tasks[task_id].start)
-        self.greenlets[task_id].link_exception(self._on_task_exception)
+        def wrapper():
+            try:
+                self.tasks[task_id].start()
+            except Exception as e:
+                self._on_task_exception(task_id, e)
+
+        self.greenlets[task_id] = gevent.spawn(wrapper)
 
         # Set running state
         self.tasks[task_id]._state = 'RUNNING'
+        self.redis.hset('{0}{1}'.format(
+            self.REDIS['TASK_PREFIX'],
+            task_id,
+        ), 'state', 'RUNNING')
 
-    # Reload a tasks data by stopping/re-init-ing/starting
     def _reload_task(self, task_id):
+        '''Reload a tasks data by stopping/re-init-ing/starting'''
         self.logger.debug('Reloading task: {0}'.format(task_id))
         self._stop_task(task_id)
 
@@ -241,31 +264,46 @@ class PyTask(object):
         self.tasks[task_id].__init__(**json.loads(task_data))
         self._start_task(task_id)
 
-    # Restart failing tasks
-    def _on_task_exception(self, greenlet):
-        # Set task ERROR state
-        print greenlet
-        self.logger.critical('GREENLET EXCEPTION')
+    def _on_task_exception(self, task_id, e):
+        '''Restart failing tasks'''
+        # Get the error details
+        error_type, value, trace = sys.exc_info()
+        self.logger.warning('Exception in task: {0}: {1} - {2}'.format(task_id, error_type.__name__, e))
+        print '------------ Traceback:'
+        print traceback.print_tb(trace)
+        print '------------'
 
-        # TODO: finish this
+        # Set error state
+        self.tasks[task_id]._state = 'ERROR'
+        self.redis.hset('{0}{1}'.format(
+            self.REDIS['TASK_PREFIX'],
+            task_id,
+        ), 'state', 'ERROR')
 
-    # Check for new tasks in Redis
     def _get_new_tasks(self):
+        '''Check for new tasks in Redis'''
         new_task_id = self.redis.rpop(self.REDIS['NEW_QUEUE'])
         if new_task_id is not None:
             self._add_task(new_task_id)
             self._get_new_tasks()
 
-    # Update RUNNING task times in Redis & cleanup old tasks
     def _update_tasks(self):
-        print self.tasks
+        '''Update RUNNING task times in Redis'''
+        update_time = time.time()
 
-        # TODO: finish this
+        for task_id, task in self.tasks.iteritems():
+            if task._state == 'RUNNING':
+                self.redis.hset('{}{}'.format(
+                    self.REDIS['TASK_PREFIX'],
+                    task_id
+                ), 'last_update', update_time)
+
+        # TODO: cleanup tasks
 
 
     ### Redis pubsub helpers
-    # Check for Redis pubsub messages, apply to matching pattern/channel subscriptions
     def _pubsub(self):
+        '''Check for Redis pubsub messages, apply to matching pattern/channel subscriptions'''
         while True:
             message = self.pubsub.get_message()
             if message and message['type'] == 'message':
@@ -281,8 +319,8 @@ class PyTask(object):
                 self._pubsub()
             gevent.sleep(.5)
 
-    # Subscribe to Redis pubsub messages
     def _subscribe(self, callback, channel=None, pattern=None):
+        '''Subscribe to Redis pubsub messages'''
         if channel is not None:
             self.channel_subscriptions.setdefault(channel, []).append(callback)
             self.pubsub.subscribe(channel)
