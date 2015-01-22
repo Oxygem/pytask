3ee6b617a89fb007937521dc3d1a68fc3d473447
Nick Barrett <nick@oxygem.com>
2015-01-22 19:02:48 +0000

    typo

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ echo "# Git tag & push..."
 git tag -a "v$VERSION" -m "v$VERSION"
 git push --tags
 
-echo "# Publishing to pypi
+echo "# Publishing to pypi"
 python setup.py sdist upload
 
 echo "# Done!"
