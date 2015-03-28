76d0a08c92824d52ebb77344e603b0ef311f45a4
Nick Barrett <nick@oxygem.com>
2015-03-28 21:12:50 +0000

    +changelog, 1.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# v1.0.2
+
++ Fixed not unsubscribing to task pubsub messages
++ Better cleanup of `pre_start` tasks
++ Added changelog
--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
 setup(
-    version = '1.0.1',
-    name = 'pytask',
-    description = 'A simple Python based task runner',
-    author = 'Nick Barrett',
-    author_email = 'nick@oxygem.com',
-    url = 'http://github.com/Oxygem/pytask',
-    py_modules = ['pytask']
+    version='1.0.2',
+    name='pytask',
+    description='A simple Python based task runner',
+    author='Nick Barrett',
+    author_email='nick@oxygem.com',
+    url='http://github.com/Oxygem/pytask',
+    py_modules=['pytask']
 )
